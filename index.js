5355d76be6dc32ff09638c9b78db0f015d31bc29
fernandogarcia2008 <fernando.garciamoscoso26@compscihigh.org>
2026-01-07 17:42:18 +0000

    DONE

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 })
 
+app.get("/seuss2", (req, res) => {
+  res.sendFile(__dirname + "/public/seuss2.html");
+})
+
+app.get("/seuss3", (req, res) => {
+  res.sendFile(__dirname + "/public/seuss3.html");
+})
+
+app.get("/christmas", (req, res) => {
+  res.sendFile(__dirname + "/public/christmas.html");
+})
+
 app.listen(3000, () => {
   console.log(`Holiday Server is Running!`)
 });
